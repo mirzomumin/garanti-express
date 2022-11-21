481b29a6b76ddcd9ea020088f882a59f3b238e7e
Diyorbek <diyorbeky610@gmail.com>
2022-11-21 17:59:57 +0500

    Add login

--- apps/seller/views.py
+++ apps/seller/views.py
@@ -1,3 +1,5 @@
+import json
+
 from rest_framework import viewsets
 from rest_framework.authtoken.models import Token
 from rest_framework.decorators import action
@@ -16,15 +18,16 @@ class SellerViewset(viewsets.ModelViewSet):
     @action(methods=['post'], detail=False)
     def post(self, request):
         data = request.data.get("data")
-        # address = data["addresses"]
+        data = json.loads(data)
+        address = data["addresses"]
         finance_contact = data["finance_contact"]
         manager_contact = data["manager_contact"]
         # print(address)
-        # address_serializer = AddressSerializer(data=address, many=True)
+        address_serializer = AddressSerializer(data=address, many=True)
         finance_contact_serializer = ContactSerializer(data=finance_contact)
         manager_contact_serializer = ContactSerializer(data=manager_contact)
-        # if not address_serializer.is_valid():
-        #     return Response(address_serializer.errors, status=HTTP_400_BAD_REQUEST)
+        if not address_serializer.is_valid():
+            return Response(address_serializer.errors, status=HTTP_400_BAD_REQUEST)
         if not finance_contact_serializer.is_valid():
             return Response(finance_contact_serializer.errors, status=HTTP_400_BAD_REQUEST)
         if not manager_contact_serializer.is_valid():
@@ -44,13 +47,13 @@ class SellerViewset(viewsets.ModelViewSet):
 
         # Create Instances
 
-        # address_serializer.save()
+        address_serializer.save()
         finance_contact_serializer.save()
         manager_contact_serializer.save()
 
         # Get id from Instances
 
-        # address_instances = [i.get('id') for i in address_serializer.data]
+        address_instances = [i.get('id') for i in address_serializer.data]
         finance_contact_instance = finance_contact_serializer.data.get('id')
         manager_contact_instance = manager_contact_serializer.data.get('id')
 
@@ -58,7 +61,7 @@ class SellerViewset(viewsets.ModelViewSet):
         token, created = Token.objects.get_or_create(user=user.last())
         data['user'] = user.last().id
         data['token'] = token.key,
-        # data['addresses'] = address_instances
+        data['addresses'] = address_instances
         data['finance_contact'] = finance_contact_instance
         data['manager_contact'] = manager_contact_instance
         seller_instance = SellerSerializer(data=data)
